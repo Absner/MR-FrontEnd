5213394acb41e64038117623decdcd0bdeb4aac0
Absner <absrraniel@gmail.com>
2017-07-20 01:07:31 -0400

    configurando ruta login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// Components
+import { LoginComponent } from './components/login/login.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    children: []
-  }
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  },
+  // {
+  //   path: '',
+  //   children: []
+  // }
 ];
 
 @NgModule({
